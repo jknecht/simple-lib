58137ca3191b175912516cdd341329c9ca613738
Jeff Knecht <jeff.knecht@slalom.com>
2015-02-21 21:04:04 -0600

    Initial hello world implementation

---
+++ src/test/java/com/slalom/simplelib/HelloWorldProviderTest.java
@@ -0,0 +1,21 @@
+package com.slalom.simplelib;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldProviderTest {
+	HelloWorldProvider provider;
+	
+	@Before
+	public void setup() {
+		provider = new HelloWorldProvider();
+	}
+	
+	@Test
+	public void shouldSayHelloToTheWorld() {
+		assertEquals("Hello, World!", provider.sayHelloWorld());
+	}
+
+}
