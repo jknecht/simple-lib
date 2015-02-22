f37543cfa2e53e4b375ee921a0efaa44d883251d
Jeff Knecht <jeff.knecht@slalom.com>
2015-02-21 22:40:58 -0600

    Javadocs

--- src/main/java/com/slalom/simplelib/HelloWorldProvider.java
+++ src/main/java/com/slalom/simplelib/HelloWorldProvider.java
@@ -2,6 +2,11 @@ package com.slalom.simplelib;
 
 public class HelloWorldProvider {
 	
+	/**
+	 * Tells the world 'hello'
+	 * 
+	 * @return
+	 */
 	public String sayHelloWorld() {
 		return "Hello, World!";
 	}
